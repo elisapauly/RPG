4f66b3d97c90b2de51543c9ef8746da1ac90c90f
Oliveira Elisa (SO/ETS-BR) <ole4jvl@bosch.com>
2025-07-14 11:27:06 -0300

    update

--- Jogo.java
+++ Jogo.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.Scanner;
 
 public class Jogo {
@@ -29,3 +30,83 @@ public class Jogo {
     }
 }
 
+=======
+public class Personagem {
+    public String nome;
+    public String classe;
+    public int nivel;
+    public int vida;
+    public int mana;
+    public int forca;
+
+    public Personagem(String nomePersonagem, String classePersonagem) {
+        this.nome = nomePersonagem;
+        this.classe = classePersonagem;
+        this.nivel = 1;
+        this.vida = 100;
+
+        switch (classe.toLowerCase()) {
+            case "mago":
+                this.mana = 100;
+                this.forca = 5;
+                break;
+            case "guerreiro":
+                this.mana = 0;
+                this.forca = 10;
+                break;
+            case "arqueiro":
+                this.mana = 20;
+                this.forca = 7;
+                break;
+            default:
+                this.mana = 0;
+                this.forca = 5;
+                break;
+        }
+    }
+
+    public void mostrarStatus() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Classe: " + classe);
+        System.out.println("Nível: " + nivel);
+        System.out.println("Vida: " + vida);
+        System.out.println("Mana: " + mana);
+        System.out.println("Força: " + forca);
+    }
+
+    public void atacar() {
+        System.out.println(nome + " atacou com " + forca + " de força!");
+    }
+
+    public void receberDano() {
+        vida -= 15;
+        if (vida < 0) vida = 0;  // Evitar vida negativa
+        System.out.println(nome + " agora tem " + vida + " de vida.");
+    }
+
+    public void usarHabilidadeEspecial() {
+        System.out.println(nome + " usou uma habilidade especial!");
+    }
+
+    public void subirNivel() {
+        nivel += 1;
+        forca += 2;
+        vida += 10;
+        System.out.println(nome + " subiu para o nível " + nivel + "!");
+    }
+
+    public void desenharPersonagem(int personagem) {
+        System.out.println("Desenhando personagem número: " + personagem);
+        System.out.println(
+            "  /^\\   \n" +
+            " (⚡_⚡)  \n" +
+            " /|_|\\  \n" +
+            "  | |   \n" +
+            " / | \\  \n" +
+            "   |    \n" +
+            "  /\\    \n" +
+            " [##]   \n"
+        );
+    }
+}
+>>>>>>> 21f59b0 (update)
--- Personagem.java
+++ Personagem.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.Scanner;
 
 public class Personagem {
@@ -129,3 +130,70 @@ public class Personagem {
 
    
 }
+=======
+public class Personagem {
+    public String nome;
+    public String classe;
+    public int nivel;
+    public int vida;
+    public int mana; // só usa se for mago
+    public int forca;
+
+    // Construtor
+    public Personagem(String nomePersonagem, String classePersonagem) {
+        this.nome = nomePersonagem;
+        this.classe = classePersonagem;
+        this.nivel = 1;
+        this.vida = 100;
+
+        switch (this.classe.toLowerCase()) {
+            case "mago":
+                this.mana = 100;
+                this.forca = 5;
+                break;
+            case "guerreiro":
+                this.forca = 10;
+                this.mana = 0;
+                break;
+            case "arqueiro":
+                this.forca = 7;
+                this.mana = 20;
+                break;
+            default:
+                this.forca = 5;
+                this.mana = 0;
+                break;
+        }
+    }
+
+    public void mostrarStatus() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Classe: " + this.classe);
+        System.out.println("Nível: " + this.nivel);
+        System.out.println("Vida: " + this.vida);
+        System.out.println("Mana: " + this.mana);
+        System.out.println("Força: " + this.forca);
+    }
+
+    public void atacar() {
+        System.out.println(this.nome + " atacou com " + this.forca + " de força!");
+    }
+
+    public void receberDano() {
+        this.vida -= 15;
+        if (this.vida < 0) this.vida = 0;
+        System.out.println(this.nome + " agora tem " + this.vida + " de vida.");
+    }
+
+    public void usarHabilidadeEspecial() {
+        System.out.println(this.nome + " usou uma habilidade especial!");
+    }
+
+    public void subirNivel() {
+        this.nivel += 1;
+        this.forca += 2;
+        this.vida += 10;
+        System.out.println(this.nome + " subiu para o nível " + this.nivel + "!");
+    }
+}
+>>>>>>> 21f59b0 (update)
